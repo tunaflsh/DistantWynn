0ef424bc26a8894e59cda05c427aaeb661089cda
tunaflsh <tunaflish.ai@gmail.com>
2026-07-31 16:50:06 +0200

    refactor: use targeted injection

--- src/main/java/me/tunaflsh/distantwynn/mixin/ChunkFilter.java
+++ src/main/java/me/tunaflsh/distantwynn/mixin/ChunkFilter.java
@@ -2,43 +2,39 @@ package me.tunaflsh.distantwynn.mixin;
 
 import java.util.List;
 
-import org.spongepowered.asm.mixin.Implements;
-import org.spongepowered.asm.mixin.Interface;
-import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import me.tunaflsh.distantwynn.api.ChunkExtractor;
 import me.tunaflsh.distantwynn.util.WynnRegions;
+import net.minecraft.client.renderer.Octree;
 import net.minecraft.client.renderer.SectionOcclusionGraph;
 import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
-import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockBox;
 
 @Mixin(SectionOcclusionGraph.class)
-@Implements(@Interface(iface = ChunkExtractor.class, prefix = "extractor$"))
-public abstract class ChunkFilter {
-	@Shadow(prefix = "original$")
-	public abstract void original$addSectionsInFrustum(
-			Frustum frustum,
-			List<SectionRenderDispatcher.RenderSection> list,
-			List<SectionRenderDispatcher.RenderSection> list2);
-
-	// @Inject(method = "addSectionsInFrustum", at = @At("HEAD"))
-	// private void init(CallbackInfo info) {
-	// }
-
-	@Intrinsic(displace = true)
-	public void extractor$addSectionsInFrustum(
-			Frustum frustum,
-			List<SectionRenderDispatcher.RenderSection> list,
-			List<SectionRenderDispatcher.RenderSection> list2) {
-		this.original$addSectionsInFrustum(frustum, list, list2);
-
-		BlockBox currentRegion = WynnRegions.getCurrent();
-		if (currentRegion != null) {
-			list.removeIf((section) -> !currentRegion.contains(section.getRenderOrigin()));
-			list2.removeIf((section) -> !currentRegion.contains(section.getRenderOrigin()));
-		}
+public class ChunkFilter {
+	@Inject(
+	method = "method_62924",
+	at = @At(
+		value = "INVOKE_ASSIGN",
+		target = "Lnet/minecraft/client/renderer/Octree$Node;getSection()Lnet/minecraft/client/renderer/chunk/SectionRenderDispatcher$RenderSection;"),
+	locals = LocalCapture.CAPTURE_FAILSOFT,
+	cancellable = true)
+	private static void onAddSectionsInFrustum(
+			final List<SectionRenderDispatcher.RenderSection> list,
+			final List<SectionRenderDispatcher.RenderSection> list2,
+			final Octree.Node node,
+			final boolean bl,
+			final int i,
+			final boolean bl2,
+			final CallbackInfo ci,
+			final SectionRenderDispatcher.RenderSection renderSection) {
+		final BlockBox currentRegion = WynnRegions.getCurrent();
+		if (currentRegion == null || renderSection == null) return;
+		if (currentRegion.contains(renderSection.getRenderOrigin())) return;
+		ci.cancel();
 	}
 }
