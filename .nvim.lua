ad8cbaa61a3246e9624b2643ccf6e87083496a8f
tunaflsh <tunaflish.ai@gmail.com>
2026-08-02 18:58:57 +0200

    build: add ./gradlew neovim task for minecraft-dev.nvim

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,11 @@
-vim.o.shadafile = vim.fn.getcwd() .. "/.nvim.shada"
+local root_dir = vim.uv.cwd()
+if not root_dir then
+  vim.notify("Failed to get the project's root directory", vim.log.levels.WARN)
+  return
+end
+
+local project_name = vim.fn.fnamemodify(root_dir, ':t')
+local hash = vim.fn.sha256(root_dir):sub(1, 16)
+local project_id = project_name .. '-' .. hash
+
+vim.o.shadafile = vim.fs.joinpath(vim.fn.stdpath('state'), 'shada', project_id .. '.shada')
